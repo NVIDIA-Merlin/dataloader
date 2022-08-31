a66acbc144d0cd0a079f0415eb43ff28c24d9a23
Ben Frederickson <github@benfrederickson.com>
2022-08-31 12:25:40 -0700

    Keep Dataloader lazy when using map operations
    Port changes from https://github.com/NVIDIA-Merlin/models/pull/572

--- merlin/loader/loader_base.py
+++ merlin/loader/loader_base.py
@@ -399,7 +399,7 @@ class LoaderBase:
                     c = (c, batch_lists)
 
                 batches[n].append(c)
-        return [self._handle_tensors(batch) for batch in batches]
+        return (self._handle_tensors(batch) for batch in batches)
 
     def _get_segment_lengths(self, num_samples):
         """
