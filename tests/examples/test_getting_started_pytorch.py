15d95d2fe594681d4c0b1ae4731d530110752b1a
Radek Osmulski <rosmulski@gmail.com>
2022-11-22 23:18:11 +1000

    add test for examples

---
+++ tests/examples/test_getting_started_pytorch.py
@@ -0,0 +1,23 @@
+from testbook import testbook
+
+
+pytest.importorskip("torch")
+
+@testbook("examples/01b-Getting-started-Pytorch", execute=False)
+def test_getting_started_pytorch(tb):
+    tb.inject(
+        """
+        import pandas as pd
+        import numpy as np
+
+        !mkdir -p /tmp/ml-25m
+        pd.DataFrame({
+            'userId': np.random.randint(0, 10, 100_000),
+            'movieId': np.random.randint(0, 10, 100_000),
+            'rating': np.random.randint(0, 5, 100_000).astype(np.float32)
+        }).to_csv('/tmp/ml-25m/ratings.csv', index=False)
+        """
+    )
+    tb.cells[4].source = "DATA_PATH = '/tmp'"
+    tb.cells[5].source = ""
+    tb.execute()
