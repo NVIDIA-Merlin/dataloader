618571afbe024bf487fc0a0eec7e7180ccc37475
Julio <jperez@nvidia.com>
2023-05-11 14:05:35 -0400

    made padding op faster and raise error

--- tests/unit/dataloader/test_padding.py
+++ tests/unit/dataloader/test_padding.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import numpy as np
 import pytest
 
 from merlin.core.dispatch import HAS_GPU, make_df
@@ -26,7 +27,10 @@ def test_padding(cpu):
     padding_size = 5
     padding_value = 0
     batch_size = 3
-    df = make_df({"a": [[1], [1, 2], [1, 2, 3], [1, 2, 3, 4]]})
+    vals = []
+    for _ in range(1000):
+        vals.append(np.random.choice(np.random.rand(10), np.random.randint(0, 5)))
+    df = make_df({"a": vals})
     dataset = Dataset(df, cpu=bool(cpu))
     dl_graph = ["a"] >> Padding(padding_size, padding_value)
     data_loader = Loader(
@@ -44,3 +48,33 @@ def test_padding(cpu):
         column = batch[0]["a"]
         assert column.values.shape[-1] == padding_size
         assert column.offsets is None
+
+
+@pytest.mark.parametrize("cpu", [None, "cpu"] if HAS_GPU else ["cpu"])
+def test_padding_size_too_small(cpu):
+    padding_size = 5
+    padding_value = 0
+    batch_size = 3
+    vals = []
+    for _ in range(1000):
+        vals.append(np.random.choice(np.random.rand(10), np.random.randint(0, 10)))
+    df = make_df({"a": vals})
+    dataset = Dataset(df, cpu=bool(cpu))
+    dl_graph = ["a"] >> Padding(padding_size, padding_value)
+    data_loader = Loader(
+        dataset,
+        batch_size=batch_size,
+        transforms=dl_graph,
+        shuffle=False,
+        device=cpu,
+    )
+    col_schema = data_loader._output_schema["a"]
+    assert col_schema.shape.as_tuple[-1] == padding_size
+    assert not col_schema.shape.is_fixed  # because we don't know the size of the final batch
+    assert not col_schema.is_ragged
+    with pytest.raises(ValueError) as exception_info:
+        for batch in data_loader:
+            column = batch[0]["a"]
+            assert column.values.shape[-1] == padding_size
+            assert column.offsets is None
+    assert "There are records in data that have more values" in str(exception_info.value)
