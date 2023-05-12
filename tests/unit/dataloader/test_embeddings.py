ec9bedf85e16112983c0c3dd2e1bf2f43c472dd6
Oliver Holworthy <oholworthy@nvidia.com>
2023-05-12 21:06:00 +0100

    Use Dictionary lookup for supplied IDs to Embedding Operator (#148)
    * Use lookup dict for Embedding operator with ids to speed up transform
    
    * Add test for embedding operator with unknown value
    
    * Update typehint for unknown_value
    
    * Remove embedding tag from input schema to embeddings tests
    
    The op now adds the embedding tag automatically, and the input schema
    cannot have both a CATEGORICAL and EMBEDDING tag
    
    * Flatten array passed  as ids to EmbeddingOperator
    
    * Add assertion for ids shape
    
    * Set default value of `embedding_index_mapping`
    
    * Correct casing of message in test

--- tests/unit/dataloader/test_embeddings.py
+++ tests/unit/dataloader/test_embeddings.py
@@ -19,6 +19,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
+from merlin.core.compat import cupy
 from merlin.core.dispatch import HAS_GPU
 from merlin.dataloader.loader_base import LoaderBase as Loader  # noqa
 from merlin.dataloader.ops.embeddings import EmbeddingOperator
@@ -27,6 +28,59 @@ from merlin.io import Dataset
 from merlin.schema import Tags
 from merlin.schema.tags import TagSet
 from merlin.table import TensorColumn, TensorTable
+
+
+def test_embeddings_invalid_ids():
+    ids = np.array(["a", "b"])
+    embeddings = np.random.rand(3, 10)
+    with pytest.raises(AssertionError) as exc_info:
+        EmbeddingOperator(
+            embeddings,
+            lookup_key="id",
+            embedding_name="id_embedding",
+            id_lookup_table=ids,
+        )
+    assert "IDs provided must match the number of embeddings" in str(exc_info.value)
+    assert "Expected IDs with shape (3,)" in str(exc_info.value)
+
+
+@pytest.mark.parametrize("unknown_value", [0, 1, np.random.uniform(size=10)])
+def test_embedding_lookup_with_unknown_value(unknown_value):
+    ids = np.array(["a", "b", "c"])
+    embeddings = np.random.rand(3, 10)
+    df = pd.DataFrame(
+        {
+            "id": ["a", "unknown"],
+            "feature": [1, 2],
+        }
+    )
+
+    dataset = Dataset(df, cpu=True)
+
+    data_loader = Loader(
+        dataset,
+        batch_size=3,
+        transforms=[
+            EmbeddingOperator(
+                embeddings,
+                lookup_key="id",
+                embedding_name="id_embedding",
+                id_lookup_table=ids,
+                unknown_value=unknown_value,
+            ),
+        ],
+        shuffle=False,
+    )
+    x, y = data_loader.peek()
+
+    assert x["id"].values.shape == (2,)
+    embedding_values = x["id_embedding"].values
+    if cupy and isinstance(embedding_values, cupy.ndarray):
+        embedding_values = embedding_values.get()
+    assert embedding_values.shape == (2, 10)
+    np.testing.assert_equal(embedding_values[0], embeddings[0])
+    np.testing.assert_equal(embedding_values[1], unknown_value)
+    assert data_loader.output_schema.column_names == ["id", "feature", "id_embedding"]
 
 
 def test_embedding_with_target():
@@ -116,7 +170,7 @@ def test_embedding_np_mmap_dl_with_lookup(tmpdir, rev_embedding_ids, np_embeddin
     dataset = dataset.repartition(10)
     schema = dataset.schema
     for col_name in cat_names:
-        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL, Tags.EMBEDDING])
+        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL])
     dataset.schema = schema
 
     data_loader = Loader(
@@ -148,7 +202,7 @@ def test_embedding_np_dl_no_lookup(tmpdir, embedding_ids, embeddings_from_datafr
     dataset = dataset.repartition(10)
     schema = dataset.schema
     for col_name in cat_names:
-        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL, Tags.EMBEDDING])
+        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL])
     dataset.schema = schema
     paths = sorted(glob.glob(f"{embeddings_from_dataframe}/*"))
     embeddings_ds = Dataset(paths)
@@ -183,7 +237,7 @@ def test_embedding_np_dl_with_lookup(tmpdir, rev_embedding_ids, embeddings_from_
     dataset = dataset.repartition(10)
     schema = dataset.schema
     for col_name in cat_names:
-        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL, Tags.EMBEDDING])
+        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL])
     dataset.schema = schema
     paths = sorted(glob.glob(f"{embeddings_from_dataframe}/*"))
     embeddings_ds = Dataset(paths)
@@ -191,7 +245,9 @@ def test_embedding_np_dl_with_lookup(tmpdir, rev_embedding_ids, embeddings_from_
     data_loader = Loader(
         dataset,
         batch_size=batch_size,
-        transforms=[EmbeddingOperator(embeddings_np, id_lookup_table=embedding_ids.to_numpy())],
+        transforms=[
+            EmbeddingOperator(embeddings_np, id_lookup_table=embedding_ids.to_numpy().ravel())
+        ],
         shuffle=False,
         device=cpu,
     )
@@ -222,7 +278,7 @@ def test_embedding_np_dl_with_lookup_ragged(
     dataset = dataset.repartition(10)
     schema = dataset.schema
     for col_name in cat_names:
-        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL, Tags.EMBEDDING])
+        schema[col_name] = schema[col_name].with_tags([Tags.CATEGORICAL])
     dataset.schema = schema
     paths = sorted(glob.glob(f"{embeddings_from_dataframe}/*"))
     embeddings_ds = Dataset(paths)
